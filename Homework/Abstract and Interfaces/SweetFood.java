c9f8dbe891ef851649fa7fd63d4cf8622639f378
VyNg <93438683+vytngn@users.noreply.github.com>
2022-09-12 11:58:26 -0400

    Delete SweetFood.java

--- Homework/Abstract and Interfaces/SweetFood.java
+++
@@ -1,8 +0,0 @@
-package SweetFood;
-/**Interface SweetFood **/
-public interface SweetFood {
-	/*sugarBoosts returns a string */
-	public abstract String sugarBoosts();
-	/*raiseCalories returns a string*/
-	public abstract String raiseCalories();
-}
