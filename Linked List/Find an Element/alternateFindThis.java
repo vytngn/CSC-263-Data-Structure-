24f8e58117ec4743e2bc38284f4e0334d2da6cad
VyNg <93438683+vytngn@users.noreply.github.com>
2022-09-20 12:09:39 -0400

    Update alternateFindThis.java

--- Linked List/Find an Element/alternateFindThis.java
+++ Linked List/Find an Element/alternateFindThis.java
@@ -1,4 +1,18 @@
 
+public class List {
+	protected Node head;
+	protected int size;
+  
+	/**Method findThis**/
+	public Node find(char findThis){
+		Node current  = head; 
+    
+    /**Alternate method using for loop **/
+    for(Node x = head; true; current=current.next){
+      if(current.info ==findThis)
+        return current;
+    }//end for loop
+	}	//end find()
+}//end class
 
 
-/**Alternate method using for loop **/
