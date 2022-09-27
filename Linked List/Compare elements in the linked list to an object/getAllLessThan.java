5fe66e477259303d780474db9d5fd8fc650e602c
VyNg <93438683+vytngn@users.noreply.github.com>
2022-09-26 22:22:22 -0400

    Create getAllLessThan.java

---
+++ Linked List/Compare elements in the linked list to an object/getAllLessThan.java
@@ -0,0 +1,10 @@
+/*
+ *
+ *
+
+Suppose that a list contains Comparable objects. Implement a method that returns 
+a new list of items that are less than some given item.  The header of the method 
+could be as follows:
+public  LList<T>  getAllLessThan( Comparable<T>  anObject)
+Write an implementation of this method for the class LList. Make sure that your 
+method does not affect the state of the original list.
