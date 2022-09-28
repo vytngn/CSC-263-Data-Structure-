f22eb0b1851c717f582a1b004488674286170e04
VyNg <93438683+vytngn@users.noreply.github.com>
2022-09-28 15:13:04 -0400

    Update findMaxAlternative.java

--- Linked List/Return the largest item in the list/findMaxAlternative.java
+++ Linked List/Return the largest item in the list/findMaxAlternative.java
@@ -1,2 +1,25 @@
 //Make findMax more general if the parameter is a List of ANY type that is Comparacble => use wildcard 
 
+
+/**Method returns the largest value in the list**/
+//Precondition: list is not an empty list 
+public static Comparable findMax(List<? extends Comparable> list){
+  
+  //Create iterator of the list 
+  Iterator<?extends Comparable> itr = list.iterator(); //points before the first element of the list
+  
+  //Initialize first element is the max 
+  Comparable max = itr.next();
+  
+  /** while(the list is not empty) { assign element to the first element to keep track of current element }
+  * if max > element, keep advance 
+  * if max < element, set current max = current element 
+  * end loop, return max 
+  */
+  while(itr.hasNext()){
+    Comparable current = itr.next();
+    if(max.compareTo(current) < 0)
+      max = current;
+  } //end while loop
+  return max;
+}
