4d8eed7a078d2954f5d950f58a011b0d81f11786
VyNg <93438683+vytngn@users.noreply.github.com>
2022-10-03 11:55:57 -0400

    Update LockADT.java

--- Labs/Lab 2 ADTs/Combination Lock/LockADT.java
+++ Labs/Lab 2 ADTs/Combination Lock/LockADT.java
@@ -1,33 +1,83 @@
 /**Lock Data Structure class**/
+public class Lock {
+private int x,y,z;
+private boolean isLock;
 
 /**Constructors**/
-
 //default constructor
-
+public Lock(){
+  int x = 0;
+  int y = 0;
+  int z = 0;
+  isLock = true;
+}
 //overloaded constructor 
+public Lock(int x, int y, int z){
+  if (x >39 || y > 39 || z > 39) throw new IllegalArgumentException("Number should be from 0 to 39.");
+        else
+        this.x = x;
+        this.y = y;
+        this.z = z;
+}
 
 //copy constructor 
+public Lock(Lock lock){
+  x = lock.x;
+  y = lock.y;
+  z = lock.z;
+}
 
-/**Accessor methods**/
-
-//set Y
+/**Mutator methods**/
 
 //set X
+public void setX(int x){
+  this.x = x;
+}
+
+//set Y
+public void setY(int y){
+  this.y = y;
+}
 
 //set Z
+public void setZ(int z){
+  this.z = z;
+}
 
 //set new combination 
+public void setNewLock(int newX, int newY, int newZ){
+  x = newX;
+  y= newY;
+  z= newZ;
+}
 
-/**Methods check the status of the lock**/
+//close the lock
+public void close(){
+  isLock = true;
+}
+//open the lock
+public void open(){
+  isLock = false;
+}
 
-/**Method to open and close the lock**/
+/**Accessor method**/
 
-//close
-//open 
-//turn
+//return true if the lock is locked 
+public boolean isLock(){
+  return isLock;
+}
 
 /**Mutator method**/ 
 
 //get current number the dial pointing at 
 
 // to String method 
+public String toString(){
+  if (isLocked = false)
+    return "Lock is open.";
+  else 
+    return "Lock is closed.";
+}
+
+
+
