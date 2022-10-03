f0a9e54d9af3b70086f393ad5764ff07debd34a1
VyNg <93438683+vytngn@users.noreply.github.com>
2022-10-03 11:29:51 -0400

    Create LockClientDemoClass.java

---
+++ Labs/Lab 2 ADTs/Combination Lock/LockClientDemoClass.java
@@ -0,0 +1,20 @@
+/** Have lock object instatiated and a menu method that displays 
+ * 1. set lock combination 
+ * 2. clock close 
+ * 3. check status 
+ * 4. attempt to open lock 
+ * 5. check what the dial is currently pointing at 
+ * 6. exit the program 
+ */
+
+//create a lock object 
+
+//display options 
+
+//option 1 called 
+//option 2 called 
+//option 3 called
+//option 3 called
+//option 4 called
+//option 5 called 
+//option 6 called 
