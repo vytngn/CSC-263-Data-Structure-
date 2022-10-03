8da9fb2ebb24f52c200b220939e06ad1768a6c3c
VyNg <93438683+vytngn@users.noreply.github.com>
2022-10-03 11:16:40 -0400

    Create LockADT.java

---
+++ Labs/Lab 2 ADTs/Combination Lock/LockADT.java
@@ -0,0 +1,33 @@
+/**Lock Data Structure class**/
+
+/**Constructors**/
+
+//default constructor
+
+//overloaded constructor 
+
+//copy constructor 
+
+/**Accessor methods**/
+
+//set Y
+
+//set X
+
+//set Z
+
+//set new combination 
+
+/**Methods check the status of the lock**/
+
+/**Method to open and close the lock**/
+
+//close
+//open 
+//turn
+
+/**Mutator method**/ 
+
+//get current number the dial pointing at 
+
+// to String method 
