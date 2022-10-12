a8b15095f5f87e230e7a5e07cd516f11e5bc0eb9
VyNg <93438683+vytngn@users.noreply.github.com>
2022-10-12 11:46:45 -0400

    Update popAndPrintStack.java

--- Stack/Methods/popAndPrintStack.java
+++ Stack/Methods/popAndPrintStack.java
@@ -3,12 +3,9 @@
 //pops each element from the stack, and prints it. 
 // It should have a type parameter that represents the Stack’s element type.
 public static <E> void popAndPrintStack(Stack<E> stack){
-    while(!(stackTop == null)){ 
-    //store reference data of the top element to a string and print it 
-    String s = stackTop.info;
-    System.out.print(s + " ");
-    //pop operation
-    stackTop = stackTop.link; //point stackTop the next link 
+    while(!stack.isEmpty()){ 
+    E item = stack.pop();
+    System.out.println(item);
     }//end while loop
 }
 
@@ -19,34 +16,22 @@ public Class Baby
      String  name;
      int  birthWeight;
 }
-
 //Write an equals method for the Baby class. 
+
 public boolean equals(Object obj){
   
-//return true if point to the same object 
-if(this == obj)
-  return true;
+    if (object == null ) 
+        return false;
   
-// if object is pointing to null, or if object not belongs  to the same class return false 
-if ((obj == null)  || this.getClass() != obj.getClass()) 
-  return false;
+    if(!(object instanceof(Baby))
+       return false;
+    if(this.name.equals(object.name) && this.birthWeigth == object.birthWeight)
+        return true;
+    else
+        return false;
+}
 
-//type casting object 
-Baby baby1 = (Baby)obj;
 
-//compare value of objects 
-return this.name.equals(baby1.name) && this.birthWeight.equals(baby1.birthWeight);
-} // end method 
-
-// Another way of equals method 
-public boolean equals(Object obj){
-  if (obj instanceof Baby){
-  //type casting 
-  Baby baby1 = (Baby)obj;
-  return name.equals(baby1.name) && birthWright.equals(baby1.birthWright);
-  } //end if
-  return false; //return false if obj is not an instance of Baby class
-} // end method 
 
 
 
