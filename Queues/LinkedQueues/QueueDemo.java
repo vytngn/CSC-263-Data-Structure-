ff9686fd14246b5980c13adfd1fe2cbdfe7df2cf
VyNg <93438683+vytngn@users.noreply.github.com>
2022-10-21 21:21:04 -0400

    Create QueueDemo.java

---
+++ Queues/LinkedQueues/QueueDemo.java
@@ -0,0 +1,21 @@
+public class QueueDemo
+{
+    public static void main(String[ ] args)
+    {
+        Queue q = new Queue( );
+
+        q.addToBack("Tom");
+        q.addToBack("Dick");
+        q.addToBack("Harriet");
+
+        while(!q.isEmpty( ))
+        {
+            System.out.println(q.whoIsNext( ));
+            q.removeFront( );
+        }
+        System.out.println("The queue is empty.");
+    }
+}
+
+
+//output – items exit the queue in the SAME order they entered the queue.
