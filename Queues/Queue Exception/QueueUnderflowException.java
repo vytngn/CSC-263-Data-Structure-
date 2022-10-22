c6662dabc3d8d31ed602e7d85b472c82f14e08c5
VyNg <93438683+vytngn@users.noreply.github.com>
2022-10-21 21:35:54 -0400

    Create QueueUnderflowException.java

---
+++ Queues/Queue Exception/QueueUnderflowException.java
@@ -0,0 +1,11 @@
+public class QueueUnderflowException extends QueueException
+{
+    public QueueUnderflowException( )
+    {
+         super("Queue Underflow");
+    }
+
+    public QueueUnderflowException(String msg)
+    {
+        super(msg);
+    }
